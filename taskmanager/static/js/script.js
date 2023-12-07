1348f0c1471523329e78d2c5b0bd0c95c91f44ec
cshimvin <cshimminvincent@gmail.com>
2023-12-07 16:25:09 +0000

    Add delete functionality to Categories

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,8 +1,11 @@
-
 document.addEventListener('DOMContentLoaded', function () {
     // sidenav initialisation
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    var modals = document.querySelectorAll(".modal");
+    console.log(modals);
+    M.Modal.init(modals);
 });
 
 // Or with jQuery
