8077b2255b9a227b41b90d770adce28b743be6a9
cshimvin <cshimminvincent@gmail.com>
2023-12-07 15:57:39 +0000

    Add edit functionality to Categories

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -13,6 +13,7 @@ def categories():
     categories = Category.query.order_by(Category.category_name).all()
     return render_template("categories.html", categories=categories)
 
+
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
     if request.method == "POST":
@@ -21,3 +22,13 @@ def add_category():
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+
+@app.route("/edit_category/<int:category_id>", methods=["POST", "GET"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
